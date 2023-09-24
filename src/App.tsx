327fa34bb0e5273d407462784a7a4e45bce67766
blatoo <connygy@googlemail.com>
2023-09-24 04:56:41 +0200

    start

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./index.css";
+function App() {
+  return (
+    <>
+      <div className="py-6 max-container">
+        <button className="custom-btn">Hello</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
